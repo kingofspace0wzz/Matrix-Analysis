266fd6d8361ef46d883bbe033c07b40cec604d2f
Prince K <zizhuang_wang@umail.ucsb.edu>
2017-06-11 01:32:47 +0800

    update

--- matrix/SpeicalMatrix.py
+++ matrix/SpeicalMatrix.py
@@ -1,7 +1,6 @@
 __author__ = 'kingofspace0wzz'
 
 import numpy as np
-
 from scipy import linalg as la
 
 # Several special matries
@@ -13,6 +12,8 @@ from scipy import linalg as la
 # Orthgonal Matrix/ Unitary Matrix
 
 # Hadamard Matrix
+
+# Simple Matrix
 
 # Householder Matrix
 
@@ -63,6 +64,40 @@ def isUnitary(A):
         return True
     else:
         return False
+
+def isSimple(A):
+
+    # check if A is a squre matrix
+    if A.shape[1] != A.shape[0]:
+        return False
+
+    eigenValues, eigenVectors = la.eig(A)
+
+    '''
+    # an array of all distinct eigen values with their associated algebraic multiplicity
+    # keys: eigen values
+    # values: algebraic multiplicity
+    # {eigenValue: algebraicMulti}
+    dictValues = {}
+    '''
+    while (eigenValues.all() != np.array([])):
+
+        #dictValues.update({eigenValues[0]: 1})
+
+        index = np.argwhere(abs(eigenValues - eigenValues[0]) < 0.00001)
+        algebraicMulti = len(index)
+
+        geometricMulti = eigenVectors[:, index].shape[1]
+
+        if algebraicMulti != geometricMulti:
+            return False
+
+        #dictValues.update({eigenValues[0]: len})
+        np.delete(eigenValues, index)
+
+    # stack another spaces of eigenvalue and eigenvector
+
+    return True
 
 
 ''' Constrct a householder matrix/transformation
